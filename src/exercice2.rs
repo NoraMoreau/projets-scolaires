e43c6a5852d04b833a23b8dd5e91f243645e6108
Nora <nora.moreau66@gmail.com>
2023-10-10 23:13:40 +0200

    TD2 exo2 fini

--- src/exercice2.rs
+++ src/exercice2.rs
@@ -1,20 +1,45 @@
 /// Implémente le crible d'Eratosthène.
 /// La fonction retourne un vecteur contenant les nombres premiers inférieurs ou égaux à `n`.
-///
+///-> Vec<u32>
 /// # Exemple
 /// ```
 /// let r = td_exercices_choisis::exercice2::sieve(7);
 /// assert_eq!(r, vec![2, 3, 5, 7])
 /// ```
 pub fn sieve(n: u32) -> Vec<u32> {
-    panic!("Not implemented!");
-    
+
+    /*panic!("Not implemented!");*/
+
+    let mut vec: Vec<u32> = Vec::new();
+    for i in 2..=n {
+        vec.push(i);
+    }
+    let mut premier: Vec<u32> = Vec::new();
+    let mut a = 1;
+
+    for i in 2..n  {
+
+        if !vec.is_empty() && a != vec[0] { //si le vecteur n'est pas vide 
+            a = vec[0];                    //et faire en sorte que a ne soit qu'une fois
+            premier.push(a);
+            println!("a vaut {}", a);
+        }
+
+        vec.retain(|&j| j % i != 0);
+
+        println!("vec vaut {:?}", vec);
+        println!("premier vaut {:?}", premier);
+    }
+
+    return premier;
+
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    #[test]
+
+   #[test]
     fn n_zero() {
         let m = sieve(0);
         assert_eq!(m.len(), 0);
